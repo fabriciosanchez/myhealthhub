ba1de682b7082c8c4fa454e493990548f240b8eb
Fabricio Sanchez <fabsanc@microsoft.com>
2022-04-15 17:05:51 -0400

    Adding patient controller

---
+++ src/myhealthhub.api/Controllers/PatientController.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using myhealthhub.api.Models;
+using myhealthhub.api.Models.Entities;
+
+namespace myhealthhub.api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private readonly MyHealthHubContext _context;
+
+        public PatientController(MyHealthHubContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("all")]
+        public async Task<ActionResult<IEnumerable<Patient>>> GetPatients()
+        {
+            return await _context.Patients.ToListAsync();
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Patient>> GetPatient(string id)
+        {
+            var patient = await _context.Patients.FindAsync(Guid.Parse(id));
+
+            if(patient == null)
+            {
+                return NotFound();
+            }
+
+            return patient;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<Patient>> PostPatient(Patient patient)
+        {
+            _context.Patients.Add(patient);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if(PatientExists(patient.Id.ToString()))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    return StatusCode(500);
+                }
+            }
+
+            return CreatedAtAction("GetPatient", new { id = patient.Id }, patient);
+        }
+
+        private bool PatientExists(string id)
+        {
+            return _context.Patients.Any(e => e.Id == Guid.Parse(id));
+        }
+    }
+}
